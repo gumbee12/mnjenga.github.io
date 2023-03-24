324f22df02dfdf76a19370b7db357392925d2e67
gumbee12~ <muganjenga254@gmail.com>
2023-03-24 15:57:12 +0300

    added-everythinh

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ var home = document.getElementById('home')
 var sendmess = document.getElementById('sendmess')
 
 const colors = ["red", "blue", "orange", "yellow", "pink"];
-var colorl = "white"
 
 function onBtnClick(){
     /*alert("Button clicked!")*/
@@ -13,21 +12,51 @@ function onBtnClick(){
 }
 function sendMessage(){
     alert("Data sent!")
-
-
 }
 btn.onclick = onBtnClick
 sendmess.onclick = sendMessage
-logol.onclick = lightTheme
 
-function lightTheme() {
-        var element = document.body;
-        element.classList.toggle("light-theme");
-        document.getElementById("logol").childNodes[0].nodeValue=
-   "New Button Text using childNodes";
-}
-function darkTheme() {
-    var element = document.body;
-    element.classList.toggle("light-theme");
+function changeWebsiteColors() {
+	var button = document.querySelector('.my-button');
+	button.classList.toggle('active');
+	
+	if (button.classList.contains('active')) {
+		document.body.style.color = '#fff';
+		
+		var divs = document.querySelectorAll('#nav-header');
+		var sections = document.querySelectorAll('section');
+        var foot = document.querySelectorAll('#nav-footer');
+		
+		for (var i = 0; i < divs.length; i++) {
+			divs[i].style.backgroundColor = 'rgb(15, 4, 61)';
+		}
+		
+		for (var i = 0; i < sections.length; i++) {
+			sections[i].style.backgroundColor = 'rgb(2, 51, 2)';
+		}
+        for (var i = 0; i < foot.length; i++) {
+			foot[i].style.backgroundColor = 'rgb(15, 4, 61)';
+            foot[i].style.color = '#fff';
+		}
+	} 
+    else {
+		document.body.style.color = '#000';
+		
+		var divs = document.querySelectorAll('#nav-header');
+		var sections = document.querySelectorAll('section');
+        var foot = document.querySelectorAll('#nav-footer');
+		
+		for (var i = 0; i < divs.length; i++) {
+			divs[i].style.backgroundColor = '#f8f9fa';
+		}
+		
+		for (var i = 0; i < sections.length; i++) {
+			sections[i].style.backgroundColor = '#f8f9fa';
+		}
+        for (var i = 0; i < foot.length; i++) {
+			foot[i].style.backgroundColor = '#f8f9fa';
+            foot[i].style.color =  '#000';
+        }
+	}
 }
 
